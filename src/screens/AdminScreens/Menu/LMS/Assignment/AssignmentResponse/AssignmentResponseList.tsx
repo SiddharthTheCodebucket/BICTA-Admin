733564cd1c8d7cce517a9dc894b94fcab25ee662
SiddharthTheCodebucket <siddharth.k@thecodebucket.com>
2026-04-17 12:34:41 +0530

    assigmnet responce fixes

---
+++ src/screens/AdminScreens/Menu/LMS/Assignment/AssignmentResponse/AssignmentResponseList.tsx
@@ -0,0 +1,397 @@
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Toast from 'react-native-toast-message';
+import { useFocusEffect } from '@react-navigation/native';
+import {
+  colors,
+  fonts,
+  screensName,
+  strings,
+  vh,
+  vw,
+} from '../../../../../../constants';
+import { SvgEye } from '../../../../../../constants/svgs';
+import { useAppSelector } from '../../../../../../hooks';
+import {
+  Header,
+  NavigationType,
+} from '../../../../../../components/organisms/HeaderOrganism';
+import TextAtom from '../../../../../../components/atoms/TextAtom';
+import FullscreenLoading from '../../../../../../components/organisms/FullscreenLoading';
+import AdminListHeader from '../../../../../../components/organisms/AdminListHeader';
+import TouchableAtom from '../../../../../../components/atoms/TouchableAtom';
+import DropDownOrganism from '../../../../../../components/organisms/DropDownOrganism';
+import { useCommonDropdownListMutation } from '../../../../../../injectEndpoints/vehicleManagemnetEndpoints';
+
+interface Props {
+  navigation: NavigationType;
+  route?: any;
+}
+
+const debounce = (func: any, delay: number) => {
+  let timer: any;
+  return (...args: any[]) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+const AssignmentResponseList = (props: Props) => {
+  const { navigation, route } = props;
+
+  const { crediantialData } = useAppSelector(state => state.Auth);
+
+  const [commonApi] = useCommonDropdownListMutation();
+
+  const [data, setData] = useState<any>([]);
+
+  const [firstTimeLoad, setFirstTimeLoad] = useState(true);
+  const [pagination, setPagination] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [initialCall, setInitialCall] = useState(false);
+
+  const [search, setSearch] = React.useState('');
+  const [centerSerach, setCenterSerach] = React.useState<any>({});
+
+  useLayoutEffect(() => {
+    if (route?.params?.suppressHeader) return;
+    Header.setNavigation(
+      navigation,
+      strings.lms.assignmentResponse.title,
+      undefined,
+      undefined,
+      undefined,
+      {
+        backgroundColor: colors.primary_dark_blue,
+        titleColor: colors.white,
+        backIconColor: colors.white,
+      },
+    );
+    navigation.BackButtonPress = () => navigation.goBack();
+  }, [navigation, route?.params?.suppressHeader]);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (firstTimeLoad && !centerSerach?.name && search === '') {
+        setFirstTimeLoad(false);
+        listAssignmentTrainingList();
+      }
+    }, [firstTimeLoad, centerSerach, search]),
+  );
+
+  useEffect(() => {
+    if (!centerSerach?.name) return;
+    listAssignmentTrainingList();
+  }, [centerSerach]);
+
+  const getCentreFilter = () => {
+    if (!centerSerach?.name) return null;
+
+    if (centerSerach.name === strings.dashboardIndex.allCenters) {
+      return [strings.dashboardIndex.gaya, strings.dashboardIndex.patna];
+    }
+
+    return [centerSerach.name];
+  };
+
+  const listAssignmentTrainingList = () => {
+    setInitialCall(true);
+
+    const centreFilter = getCentreFilter();
+
+    const searchValue = search?.trim() ? `%${search.trim()}%` : `%%`;
+
+    const params: any = {
+      listType: 'assessment_assignment_training_name_list',
+      bipardCentre: [],
+      replacements: [searchValue],
+    };
+    if (centreFilter) {
+      params.bipardCentre = centreFilter;
+    }
+
+    commonApi(params)
+      .unwrap()
+      .then((res: any) => {
+        const newData = res?.data ?? [];
+        setInitialCall(false);
+        setPagination(false);
+        setRefreshing(false);
+
+        setData(newData);
+      })
+      .catch((err: any) => {
+        setInitialCall(false);
+        setPagination(false);
+        setRefreshing(false);
+        Toast.show({
+          type: 'error',
+          text2: err.data?.message || 'Something went wrong',
+        });
+      });
+  };
+
+  const handleSearch = useCallback(
+    debounce((text: string) => {
+      listAssignmentTrainingList();
+    }, 500),
+    [],
+  );
+
+  const onChangeSearch = (text: string) => {
+    setSearch(text);
+    handleSearch(text);
+  };
+
+  const onClearSearch = () => {
+    setSearch('');
+    listAssignmentTrainingList();
+  };
+
+  const AssignmentResponseCard = ({ item, index, navigation }: any) => {
+    return (
+      <TouchableAtom
+        style={styles.card}
+        onPress={() => {
+          navigation.navigate(screensName.AssignmentRessponseDetails, {
+            item: item,
+          });
+        }}
+      >
+        <View style={styles.cardContent}>
+          <View>
+            <TextAtom style={styles.title}>{item.name ?? '-'}</TextAtom>
+            <TextAtom style={styles.subtitle}>
+              {strings.lms.assignmentResponse.totalAssignment} -{' '}
+              {item.totalAssignment ?? '-'}
+            </TextAtom>
+          </View>
+          <SvgEye />
+        </View>
+      </TouchableAtom>
+    );
+  };
+
+  const renderItem = ({ item, index }: any) => {
+    return (
+      <AssignmentResponseCard
+        item={item}
+        index={index}
+        navigation={navigation}
+      />
+    );
+  };
+
+  return (
+    <SafeAreaView edges={['bottom']} style={styles.container}>
+      <FullscreenLoading isVisible={initialCall} />
+
+      <AdminListHeader
+        config={{
+          title: strings.lms.assignmentResponse.title,
+          count: data.length,
+          showCount: true,
+          search: {
+            visible: true,
+            onPress: () => {
+              // Implementation for search can be added here
+            },
+          },
+          filter: {
+            visible: false,
+          },
+          create: {
+            visible: false,
+          },
+        }}
+      />
+
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        ListEmptyComponent={
+          initialCall ? null : (
+            <TextAtom style={styles.emptyText}>
+              {strings.lms.assignmentResponse.noDataFound}
+            </TextAtom>
+          )
+        }
+        ListFooterComponent={
+          <ActivityIndicator
+            size={'small'}
+            color={colors.primary}
+            animating={pagination}
+            style={styles.marginTop15}
+          />
+        }
+        refreshControl={
+          <RefreshControl
+            tintColor={colors.primary}
+            colors={[colors.primary]}
+            refreshing={refreshing}
+            onRefresh={() => {
+              setRefreshing(true);
+              listAssignmentTrainingList();
+            }}
+          />
+        }
+        contentContainerStyle={styles.flatListContainer}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default AssignmentResponseList;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: colors.new_ui_screen_bg },
+  flatListContainer: {
+    paddingVertical: vh(10),
+  },
+  card: {
+    backgroundColor: colors.white,
+    marginHorizontal: vw(15),
+    borderRadius: vw(10),
+    padding: vw(16),
+    marginBottom: vh(12),
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOpacity: 0.15,
+    shadowRadius: 4,
+    shadowOffset: { width: 0, height: 1 },
+  },
+  cardContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  title: {
+    fontSize: vw(14),
+    fontFamily: fonts.Inter_Medium,
+    color: '#2D2D2D',
+  },
+  subtitle: {
+    fontSize: vw(12),
+    fontFamily: fonts.Inter_Regular,
+    color: colors.grey,
+    marginTop: 4,
+  },
+  emptyText: {
+    textAlign: 'center',
+    marginTop: vh(50),
+    color: colors.grey,
+    fontFamily: fonts.Roboto_Medium,
+  },
+  rowBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  thumbnail: {
+    width: 70,
+    height: 70,
+    backgroundColor: colors.lightGray2,
+    borderRadius: 8,
+    marginTop: 6,
+  },
+  statusBox: {
+    marginTop: vh(8),
+    paddingVertical: vh(8),
+    paddingHorizontal: vw(12),
+    borderRadius: vw(6),
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  activeBox: {
+    backgroundColor: colors.lightGreenBg,
+    borderColor: colors.darkGreen,
+  },
+  inActiveBox: {
+    backgroundColor: colors.lightRedBg,
+    borderColor: colors.darkRed,
+  },
+  statusText: {
+    fontFamily: fonts.Roboto_Medium,
+    fontSize: vw(14),
+  },
+  activeText: { color: colors.greenText },
+  inActiveText: { color: colors.redText },
+  dropMenu: {
+    marginTop: vh(6),
+    backgroundColor: colors.white,
+    borderWidth: 1,
+    borderColor: colors.grey,
+    borderRadius: vw(6),
+    overflow: 'hidden',
+  },
+  dropItem: {
+    paddingVertical: vh(10),
+    paddingHorizontal: vw(12),
+    borderBottomWidth: 1,
+    borderBottomColor: colors.chinese_silver,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+    zIndex: 998,
+  },
+  filterButton: {
+    borderWidth: vw(1),
+    borderColor: colors.primary,
+    borderRadius: vw(4),
+    marginTop: vh(10),
+    alignSelf: 'flex-end',
+    marginRight: vh(15),
+    paddingHorizontal: vw(10),
+    paddingVertical: vh(5),
+  },
+  filterText: {
+    color: colors.black,
+    fontFamily: fonts.Roboto_Medium,
+    fontSize: vw(14),
+  },
+  filterContainer: { paddingHorizontal: vw(15) },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: vh(5),
+  },
+  applyBtn: { width: vw(150), height: vh(35) },
+  clearBtn: {
+    width: vw(150),
+    height: vh(35),
+    borderWidth: vw(1),
+    borderColor: colors.primary,
+    backgroundColor: colors.white,
+  },
+  marginTop15: {
+    marginTop: vh(15),
+  },
+  separator: {
+    height: vh(10),
+  },
+});
