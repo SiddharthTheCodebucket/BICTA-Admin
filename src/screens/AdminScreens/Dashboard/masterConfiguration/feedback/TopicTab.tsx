532d6431c0b1cdf940022c4d43dcdc330758482a
SiddharthTheCodebucket <siddharth.k@thecodebucket.com>
2026-04-23 11:04:27 +0530

    add CategoryTab, SubCategoryTab, and TopicTab components for feedback section

---
+++ src/screens/AdminScreens/Dashboard/masterConfiguration/feedback/TopicTab.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { colors, fonts, vw } from '../../../../../../constants';
+
+const TopicTab = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Feedback Topic List</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.new_ui_screen_bg,
+  },
+  text: {
+    fontFamily: fonts.Inter_Medium,
+    fontSize: vw(16),
+    color: colors.text_black,
+  },
+});
+
+export default TopicTab;
